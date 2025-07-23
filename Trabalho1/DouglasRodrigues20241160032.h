6c18581ed86fef4a12198e866e1696fb3d99ab01
Douglas Rodrigues <douglas0sr@gmail.com>
2025-07-23 20:30:56 -0300

    Update DouglasRodrigues20241160032.h

--- Trabalho1/DouglasRodrigues20241160032.h
+++ Trabalho1/DouglasRodrigues20241160032.h
@@ -1,19 +1,19 @@
 // #################################################
 //  Instituto Federal da Bahia
 //  Salvador - BA
-//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
-//  Disciplina: INF029 - Laboratório de Programação
+//  Curso de AnÃ¡lise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - LaboratÃ³rio de ProgramaÃ§Ã£o
 //  Professor: Renato Novais - renato@ifba.edu.br
 
-//  ----- Orientações gerais -----
-//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  ----- OrientaÃ§Ãµes gerais -----
+//  DescriÃ§Ã£o: esse arquivo deve conter as questÃµes do trabalho do aluno.
 //  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
-//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+//  O aluno deve preencher seus dados abaixo, e implementar as questÃµes do trabalho
 
 //  ----- Dados do Aluno -----
 //  Nome: Douglas dos Santos Rodrigues
 //  email: douglas0sr@gmail.com
-//  Matrícula: 20241160032
+//  MatrÃ­cula: 20241160032
 //  Semestre: 2025.1
 
 
@@ -22,10 +22,10 @@
 
 typedef struct DQ
 {
-    int iDia; 
+    int iDia;
     int iMes;
     int iAno;
-    int valido; // 0 se inválido, e 1 se válido 
+    int valido; // 0 se invÃ¡lido, e 1 se vÃ¡lido
 
 } DataQuebrada;
 
@@ -38,14 +38,15 @@ typedef struct Qtd
 
 } DiasMesesAnos;
 
-int somar(int x, int y); //função utilizada para testes
-int fatorial(int x); //função utilizada para testes
+int somar(int x, int y); //funÃ§Ã£o utilizada para testes
+int fatorial(int x); //funÃ§Ã£o utilizada para testes
 int q1(char data[]);
 DiasMesesAnos q2(char datainicial[], char datafinal[]);
 int q3(char *texto, char c, int isCaseSensitive);
 int q4(char *strTexto, char *strBusca, int posicoes[30]);
 int q5(int num);
 int q6(int numerobase, int numerobusca);
+int q7(char matriz[8][10], char palavra[5]);
 DataQuebrada quebraData(char data[]);
 
 #endif  // TRABALHO1_DOUGLASRODRIGUES20241160032_H
